ebdb6bf43918e7d7f8565ae20dd7dc6b096e5a31
crowwraith <1185615@student.roc-nijmegen.nl>
2026-06-10 10:22:57 +0200

    Update postStemp.jsx

--- src/pages/posts/postStemp.jsx
+++ src/pages/posts/postStemp.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase/config";
 
+
 export default function PostStemp() {
     const [posts, setPosts] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -30,8 +31,8 @@ export default function PostStemp() {
     if (loading) {
         return <p>Posts laden...</p>;
     }
-
     return (
+
         <div className="p-4">
             <h1 className="text-2xl font-bold mb-4">Posts</h1>
 
@@ -41,9 +42,11 @@ export default function PostStemp() {
                     className="bg-zinc-900 p-4 rounded-lg mb-4"
                 >
                     <h2 className="font-bold">
-                        {post.cemeteryId}
+                        <p>naam begraafplaats:</p>
+                        <p> {post.cemeteryId}</p>
                     </h2>
 
+                    <p>beschrijving: </p>
                     <p>{post.description}</p>
 
                     <div className="mt-2">
@@ -52,12 +55,36 @@ export default function PostStemp() {
                         <p>Architectuur: {post.ratings.architecture}/5</p>
                         <p>Uniekheid: {post.ratings.uniqueness}/5</p>
                     </div>
+                    <div className='posts-AverageRating'>
+                        <button>
+                            Gemiddelde: {(
+                                (
+                                    (post.ratings?.environment ?? 0) +
+                                    (post.ratings?.peace ?? 0) +
+                                    (post.ratings?.architecture ?? 0) +
+                                    (post.ratings?.uniqueness ?? 0)
+                                ) / 4
+                            ).toFixed(2)}/5
+                        </button>
+                    </div>
 
                     <p className="text-sm text-gray-400 mt-2">
                         Likes: {post.likes.length}
                     </p>
+                    <div className='posts-Ratings'>
+                        <button className='average-Score'>like geven</button>
+                        <button className='average-Score'>likes stelen</button>
+                        {/* 1x per dag iemand likes laten stelen van deze post, of 1x per dag 1 user likes laten stelen van een post */}
+                        <button className='average-Score'>comments</button>
+                        {/* comments button aanklikken wordt gebruikt om comments visueel te maken, met bovenin de optie om zelf een comment te plaatsen,
+                        info die daarvoor nodig is staat bijna allemaal al in state, alleen comment-tekst nog en een check voor de inlog,
+                        en anders doorverwijzen naar login.  */}
+                        <button className='average-Score'>flag post</button>
+                    </div>
+                    <div className='posts-Divider'></div>
                 </div>
             ))}
         </div>
+
     );
 }
