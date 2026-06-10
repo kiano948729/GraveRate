b8cce971b1075091ff5b9dbfd9d9c6b37489a0ad
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2026-06-10 09:42:24 +0200

    hotifx

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -11,6 +11,7 @@ import Groups from "../pages/Groups/Groups";
 import GroupDetail from "../pages/Groups/GroupDetail";
 import ProtectedRoute from "./ProtectedRoute";
 import Notifications from "../pages/Notifications/Notifications";
+import UserProfile from "../pages/User/UserProfile";
 
 export const router = createBrowserRouter([
   {
@@ -23,6 +24,7 @@ export const router = createBrowserRouter([
       { path: "group/:id", element: <GroupDetail /> },
       { path: "login", element: <Login /> },
       { path: "register", element: <Register /> },
+      { path: "user/:uid", element: <UserProfile /> },
       {
         path: "profile",
         element: (
